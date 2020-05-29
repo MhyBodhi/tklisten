ef0a8b3acbc49b41c4ec510e1f55a4b3d7d42ead
dys12345678 <2436810573@qq.com>
2020-05-30 02:14:42 +0800

    木火应v1

--- bak/test.py
+++
@@ -1,8 +0,0 @@
-import os
-name = "木火应"
-filepath = "C:\\userfiles\\" + name + "\\"
-if not os.path.exists("C:\\userfiles"):
-    os.mkdir("C:\\userfiles")
-if not os.path.exists(filepath):
-    os.mkdir(filepath)
-print(os.path.exists(filepath))
